96ce0bb768ae010fc2a3d8820c22161f28846c48
HarchevnikovDenis <harchevnikovdenis@mail.ru>
2019-10-18 17:46:22 +0300

    the field is generated after the first move and the cells open when pressed

--- Assets/Scripts/BuildMap.cs
+++ Assets/Scripts/BuildMap.cs
@@ -6,6 +6,13 @@ public class BuildMap : MonoBehaviour
 {
     [SerializeField] private GameObject cellPrefab;
     private Vector3 startSpawnPosition;
+    private int count = 0;
+    public static bool[,] isOpen;
+    private GameObject[,] cells;
+    private int height;
+    private int width;
+    public static bool isFirstStepDone;
+    private string[,] field = null;
 
     private void Awake()
     {
@@ -14,36 +21,62 @@ public class BuildMap : MonoBehaviour
         GameOptions.MinesCount = 99;
         startSpawnPosition = Vector3.zero;
 
-        BuildField(GameOptions.Height, GameOptions.Width);
-    }
+        height = GameOptions.Height;
+        width = GameOptions.Width;
 
-    private void BuildField(int height, int width)
-    {
-        char[,] field = new char[height, width];
-
-        Debug.Log(GameOptions.MinesCount.ToString());
-        field = RandomBoom(height, width);
-
-        for(int i = 0; i < height; i++)
-        {
-            for(int j = 0; j < width; j++)
-            {
-                Vector3 offset = new Vector3(1.6f * j, 0, -1.6f * i);
-                GameObject cell = Instantiate(cellPrefab, startSpawnPosition + offset, Quaternion.identity);
-                if (field[i, j] == '*')
-                    cell.GetComponent<MeshRenderer>().material.color = Color.red;
-            }
-        }
-    }
-
-    private char[,] RandomBoom(int height, int width)
-    {
-        char[,] field = new char[height, width];
+        isOpen = new bool[height, width];
         for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
             {
-                field[i, j] = ' ';
+                isOpen[i, j] = false;
+            }
+        }
+
+        field = new string[height, width]; for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                field[i, j] = string.Empty;
+            }
+        }
+        cells = new GameObject[height, width];
+
+        BuildField();
+    }
+
+    public void FirstStep()
+    {
+        field = RandomBoom();
+        field = MinesCounter();
+    }
+
+    private void BuildField()
+    {
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                Vector3 offset = new Vector3(1.55f * j, 0, -1.55f * i);
+                cells[i, j] = Instantiate(cellPrefab, startSpawnPosition + offset, Quaternion.identity);
+
+                /*if (field[i, j] == "*")
+                    cells[i ,j].GetComponent<MeshRenderer>().material.color = Color.red;*/
+                //cells[i, j].GetComponent<Cell>().minesText.text = field[i, j].ToString();
+
+                cells[i, j].GetComponent<Cell>().i = i;
+                cells[i, j].GetComponent<Cell>().j = j;
+            }
+        }
+    }
+
+    private string[,] RandomBoom()
+    {
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                field[i, j] = "";
             }
         }
         int mines = 0;
@@ -54,10 +87,12 @@ public class BuildMap : MonoBehaviour
             {
                 for (int j = 0; j < width; j++)
                 {
+                    if (isOpen[i, j])
+                        continue;
                     float chance = Random.Range(0.0f, 1.0f);
-                    if (chance >= 0.95f)
+                    if (chance >= 0.9f)
                     {
-                        field[i, j] = '*';
+                        field[i, j] = "*";
                         mines++;
 
                         if (mines == GameOptions.MinesCount)
@@ -70,5 +105,53 @@ public class BuildMap : MonoBehaviour
             }
         }
         return field;
+    }
+
+    private string[,] MinesCounter()
+    {
+        for (int i = 0; i < field.GetLength(0); i++)
+        {
+            for (int j = 0; j < field.GetLength(1); j++)
+            {
+                if (field[i, j] != "*")
+                {
+                    count = 0;
+                    CountMines(i - 1, j - 1, field);
+                    CountMines(i - 1, j, field);
+                    CountMines(i - 1, j + 1, field);
+                    CountMines(i, j - 1, field);
+                    CountMines(i, j + 1, field);
+                    CountMines(i + 1, j - 1, field);
+                    CountMines(i + 1, j, field);
+                    CountMines(i + 1, j + 1, field);
+                    field[i, j] = count.ToString();
+                }
+                /*cells[i, j].GetComponent<Cell>().minesText.text = field[i, j].ToString();
+                if (field[i, j] == "*")
+                    cells[i, j].GetComponent<MeshRenderer>().material.color = Color.red;*/
+            }
+        }
+        return field;
+    }
+
+    private void CountMines(int i, int j, string[,] field)
+    {
+        try
+        {
+            if (field[i, j] == "*")
+                count++;
+            else
+                return;
+        }
+        catch (System.IndexOutOfRangeException)
+        {
+            return;
+        }
+        
+    }
+
+    public void OpenNewCell(int i, int j)
+    {
+        cells[i, j].GetComponent<Cell>().minesText.text = field[i, j].ToString();
     }
 }
--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cell : MonoBehaviour
 {
+    public Text minesText;
     public Color hoverColor;
     private Color standartColor;
+    public Color openColor;
+
+    public int i;
+    public int j;
 
     private void Start()
     {
@@ -14,11 +20,34 @@ public class Cell : MonoBehaviour
 
     private void OnMouseEnter()
     {
-        gameObject.GetComponent<MeshRenderer>().material.color = hoverColor;
+        if(!BuildMap.isOpen[i, j])
+            gameObject.GetComponent<MeshRenderer>().material.color = hoverColor;
     }
 
     private void OnMouseExit()
     {
-        gameObject.GetComponent<MeshRenderer>().material.color = standartColor;
+        if (!BuildMap.isOpen[i, j])
+            gameObject.GetComponent<MeshRenderer>().material.color = standartColor;
+    }
+
+    private void OnMouseDown()
+    {
+        if (!BuildMap.isOpen[i, j])
+        {
+            BuildMap.isOpen[i, j] = true;
+
+            BuildMap map = FindObjectOfType<BuildMap>();
+            if (!BuildMap.isFirstStepDone)
+            {
+                BuildMap.isFirstStepDone = true;
+                if (map)
+                {
+                    map.FirstStep();
+                }
+            }
+            map.OpenNewCell(i, j);
+
+            gameObject.GetComponent<MeshRenderer>().material.color = openColor;
+        }
     }
 }
